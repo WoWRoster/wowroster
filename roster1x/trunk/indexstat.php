7a09b322bc2cd344fd27c749acc6e90f73a40145
anthonyb <anthonyb@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2006-08-24 09:36:20 +0000

    Roster 1x (trunk): moved stats from the indexalt.php (Alternate) to indexstat.php except for money so we have a single "Stats" list. indexalt.php to be removed in near future.

--- roster1x/trunk/indexstat.php
+++ roster1x/trunk/indexstat.php
@@ -25,11 +25,29 @@ include_once (ROSTER_BASE.'roster_header.tpl');
 // Additional querries needed for this page
 // Make sure the last item in this array DOES NOT have a (,) at the end
 $additional_sql = array(
+	'`players`.`health`, ',
+	"IF( `players`.`health` IS NULL OR `players`.`health` = '', 1, 0 ) AS 'hisnull', ",
+	'`players`.`mana`, ',
+	"IF( `players`.`mana` IS NULL OR `players`.`mana` = '', 1, 0 ) AS 'misnull', ",
 	"`stat_int_c`, ",
 	"`stat_agl_c`, ",
 	"`stat_sta_c`, ",
 	"`stat_str_c`, ",
-	"`stat_spr_c` ",
+	"`stat_spr_c`, ",
+	'`players`.`stat_armor`, ',
+	'`players`.`stat_armor_c`, ',
+	'`players`.`stat_armor_b`, ',
+	'`players`.`stat_armor_d`, ',
+	"IF( `players`.`stat_armor_c` IS NULL OR `players`.`stat_armor_c` = '', 1, 0 ) AS 'aisnull', ",
+	'`players`.`mitigation`, ',
+	'`players`.`dodge`, ',
+	"IF( `players`.`dodge` IS NULL OR `players`.`dodge` = '', 1, 0 ) AS 'disnull', ",
+	'`players`.`parry`, ',
+	"IF( `players`.`parry` IS NULL OR `players`.`parry` = '', 1, 0 ) AS 'pisnull', ",
+	'`players`.`block`, ',
+	"IF( `players`.`block` IS NULL OR `players`.`block` = '', 1, 0 ) AS 'bisnull', ",
+	'`players`.`crit`, ',
+	"IF( `players`.`crit` IS NULL OR `players`.`crit` = '', 1, 0 ) AS 'cisnull' ",
 );
 
 $FIELD[] = array (
@@ -115,6 +133,63 @@ $FIELD[] = array (
 	),
 );
 
+$FIELD[] = array (
+	'health' => array (
+		'lang_field' => 'health',
+		'order' => array( 'hisnull','`players`.`health` DESC' ),
+		'order_d' => array( 'hisnull','`players`.`health` ASC' ),
+	),
+);
+
+$FIELD[] = array (
+	'mana' => array(
+		'lang_field' => 'mana',
+		'order' => array( 'misnull','`players`.`mana` DESC' ),
+		'order_d' => array( 'misnull','`players`.`mana` ASC' ),
+	),
+);
+
+$FIELD[] = array (
+	'stat_armor_c' => array(
+		'lang_field' => 'armor',
+		'order' => array( 'aisnull','`players`.`stat_armor_c` DESC' ),
+		'order_d' => array( 'aisnull','`players`.`stat_armor_c` ASC' ),
+		'value' => 'armor_value',
+	),
+);
+
+
+$FIELD[] = array (
+	'dodge' => array(
+		'lang_field' => 'dodge',
+		'order' => array( 'disnull','`players`.`dodge` DESC' ),
+		'order_d' => array( 'disnull','`players`.`dodge` ASC' ),
+	),
+);
+
+$FIELD[] = array (
+	'parry' => array(
+		'lang_field' => 'parry',
+		'order' => array( 'pisnull','`players`.`parry` DESC' ),
+		'order_d' => array( 'pisnull','`players`.`parry` ASC' ),
+	),
+);
+
+$FIELD[] = array (
+	'block' => array(
+		'lang_field' => 'block',
+		'order' => array( 'bisnull','`players`.`block` DESC' ),
+		'order_d' => array( 'bisnull','`players`.`block` ASC' ),
+	),
+);
+
+$FIELD[] = array (
+	'crit' => array(
+		'lang_field' => 'crit',
+		'order' => array( 'cisnull','`players`.`crit` DESC' ),
+		'order_d' => array( 'cisnull','`players`.`crit` ASC' ),
+	),
+);
 
 /**
  * Controls Output of the Total Stats value Column
