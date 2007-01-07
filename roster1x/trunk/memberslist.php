76edfd58fa2102d09e3d53603701d4d12fa79e93
zanix <zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-01-07 00:23:09 +0000

    Roster 1x: - Changed level cap stuff to 70 - Edited the professions page to show level/maxlevel since I don't know the max level of skills in TBC

--- roster1x/trunk/memberslist.php
+++ roster1x/trunk/memberslist.php
@@ -692,7 +692,7 @@ function level_value ( $row )
 		$tooltip .= '</table>';
 
 
-		if( $row['level'] == '60' )
+		if( $row['level'] == '70' )
 		{
 			$tooltip = makeOverlib($wordings[$roster_conf['roster_lang']]['max_exp'],'','',2,'',',WRAP');
 		}
@@ -704,7 +704,7 @@ function level_value ( $row )
 
 	if( $roster_conf['index_level_bar'] )
 	{
-		$percentage = round(($row['level']/60)*100);
+		$percentage = round(($row['level']/70)*100);
 
 		$cell_value .= '<div '.$tooltip.' style="cursor:default;"><div class="levelbarParent" style="width:70px;"><div class="levelbarChild">'.$row['level'].'</div></div>';
 		$cell_value .= '<table class="expOutline" border="0" cellpadding="0" cellspacing="0" width="70">';
