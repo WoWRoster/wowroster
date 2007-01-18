1c744d2c9b2a470b3375cbb19f76def1c9f57a23
zanix <zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-01-18 04:26:49 +0000

    rosterdf: - Fixed talentpoint detection

--- wowrosterdf/trunk/modules/WoWRosterDF/lib/wowdb.php
+++ wowrosterdf/trunk/modules/WoWRosterDF/lib/wowdb.php
@@ -2761,7 +2761,7 @@ class wowdb
 		$this->add_value( 'level', $data['Level'] );
 		$this->add_value( 'server', $data['Server'] );
 
-		if( !empty($data['TalentPoints']) )
+		if( isset($data['TalentPoints']) )
 			$this->add_value( 'talent_points', $data['TalentPoints'] );
 
 		$this->add_value( 'money_c', $data['Money']['Copper'] );
