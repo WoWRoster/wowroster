2852c0818445eedb573f4c714175445617f4fcb5
anaxent <anaxent@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-02-20 05:08:53 +0000

    "WoWRosterDF - Key_bc - Updated and Added"

---
+++ wowrosterdf/trunk/modules/WoWRosterDF/addons/key_bc/lang/deDE.php
@@ -0,0 +1,130 @@
+<?php
+/*	tank to daddelkopp */
+/*********************DE*************************************************/
+
+$wordings['deDE']['kbc_title_addon'] = 'Schlüssel BC';
+$inst_keys_bc['deDE']['A'] = array(
+	'SL' => array( 'Key-Only', 'SL' =>
+			'Schlüssel des Schattenlabyrinths|57267'
+		),
+	'SH' => array( 'Quests', 'ZH' =>
+			'Schlüssel der zerschmetterten Hallen|56320',
+			'Zugang zur Zitadelle|',
+			'Großmeister Dumphry|',
+			'Dumphrys Bitte|',
+			'Heißer als die Hölle|'
+		),
+	'Arcatraz' => array( 'Quests', 'Arkatraz' =>
+			'Schlüssel zu Arkatraz|58724',
+			'Kristallsammlung des Konsortiums|',
+			'Ein Hügel voll Astraler|',
+			'Sphärenräuber Nesaad|',
+			'Bitte um Unterstätzung|',
+			'Rechtmäßiger Besitz|',
+			'Eine Audienz beim Prinzen|',
+			'Triangulationspunkt Eins|',
+			'Triangulationspunkt Zwei|',
+			'Das volle Dreieck|',
+			'Sonderlieferung nach Shattrath|',
+			'Wie man in Arkatraz einbricht|'
+		),
+	'TK' => array( 'Quests', 'Karazhan' => 
+			'Der Schlüssel des Meisters|58887',
+			'Arkane Störungen|',
+			'Rastlose Aktivität|',
+			'Dalarankontaktpersonen||MS',
+			'Khadgar|',
+			'Nach Karazhan|',
+			'Das zweite und das dritte Fragment|',
+			'Die Berührung des Meisters|',
+			'Rückkehr zu Khadgar|',
+		),
+	'Kara' => array( 'Quests', 'Auge' =>
+			'Schlüssel der Stürme|58724',
+			'Die Prüfung der Naaru: Erbarmen|',
+			'Die Prüfung der Naaru: Stärke|',
+			'Die Prüfung der Naaru: Zuverlässigkeit|',
+			'Sonderlieferung nach Shattrath|',
+			'Der Schlüssel der Stürme|' 
+		),
+	'HA' => array( 'Key-Only', 'hA' =>
+			'Schlüssel der Auchenai|55216'
+		 ),
+	'HHC' => array( 'Key-Only', 'hHZ' => 
+			'Flammengeschmiedeter Schlüssel|55262'
+		), 
+	'HCR' => array( 'Key-Only', 'Kessel' =>
+			'Schlüssel des Kessels|55139'
+		),
+	'HTK' => array( 'Key-Only', 'hFdS' =>
+			'Warpgeschmiedeter Schlüssel|55231'
+		),
+	'HCoT' => array( 'Key-Only', 'HdZ' =>
+			'Schlüssel der Zeit|54481'
+		 ),
+);
+
+
+// HORDE KEYS
+$inst_keys_bc['deDE']['H'] = array(
+	'SL' => array( 'Key-Only', 'SL' =>
+			'Schlüssel des Schattenlabyrinths|57267'
+		),
+	'SH' => array( 'Quests', 'ZH' =>
+			'Schlüssel der zerschmetterten Hallen|56320',
+			'Zugang zur Zitadelle|',
+			'Großmeister Dumphry|',
+			'Dumphrys Bitte|',
+			'Heißer als die Hölle|'
+		),
+	'Arcatraz' => array( 'Quests', 'Arkatraz' =>
+			'Schlüssel zu Arkatraz|58724',
+			'Kristallsammlung des Konsortiums|',
+			'Ein Hügel voll Astraler|',
+			'Sphärenräuber Nesaad|',
+			'Bitte um Unterstützung|',
+			'Rechtmäßiger Besitz|',
+			'Eine Audienz beim Prinzen|',
+			'Triangulationspunkt Eins|',
+			'Triangulationspunkt Zwei|',
+			'Das volle Dreieck|',
+			'Sonderlieferung nach Shattrath|',
+			'Wie man in Arkatraz einbricht|'
+		),
+	'TK' => array( 'Quests', 'Karazhan' => 
+			'Der Schlüssel des Meisters|58887',
+			'Arkane Störungen|',
+			'Rastlose Aktivität|',
+			'Dalarankontaktpersonen||MS',
+			'Khadgar|',
+			'Nach Karazhan|',
+			'Das zweite und das dritte Fragment|',
+			'Die Berührung des Meisters|',
+			'Rückkehr zu Khadgar|',
+		),
+	'Kara' => array( 'Quests', 'Auge' =>
+			'Schlüssel der Stürme|58724',
+			'Die Prüfung der Naaru: Erbarmen|',
+			'Die Prüfung der Naaru: Stärke|',
+			'Die Prüfung der Naaru: Zuverlässigkeit|',
+			'Sonderlieferung nach Shattrath|',
+			'Der Schlüssel der Stürme|' 
+		),
+	'HA' => array( 'Key-Only', 'hA' =>
+			'Schlüssel der Auchenai|55216'
+		 ),
+	'HHC' => array( 'Key-Only', 'hHZ' => 
+			'Flammengeschmiedeter Schlüssel|55262'
+		), 
+	'HCR' => array( 'Key-Only', 'hEK' =>
+			'Schlüssel des Kessels|55139'
+		),
+	'HTK' => array( 'Key-Only', 'hFdS' =>
+			'Warpgeschmiedeter Schlüssel|55231'
+		),
+	'HCoT' => array( 'Key-Only', 'hHdZ' =>
+			'Schlüssel der Zeit|54481'
+		 ), 
+);
+
+?>
---
+++ wowrosterdf/trunk/modules/WoWRosterDF/addons/key_bc/localization.php
@@ -0,0 +1,29 @@
+<?php
+/******************************
+ * WoWRoster.net  Roster
+ * Copyright 2002-2006
+ * Licensed under the Creative Commons
+ * "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * Short summary
+ *  http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * Full license information
+ *  http://creativecommons.org/licenses/by-nc-sa/2.5/legalcode
+ * -----------------------------
+ *
+ * $Id$
+ *
+ ******************************/
+
+$wordings['addoncredits']['Key bc v0.1'] = array(
+	array(	"name"=>	"Titan99",
+		"info"=>	"look a key bc"),
+);
+
+include_once($addonDir.'lang/frFR.php');
+include_once($addonDir.'lang/deDE.php');
+include_once($addonDir.'lang/enUS.php');
+
+
+?>
