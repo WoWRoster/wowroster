13392645c9f41b72288c8bff482607561fdb6db3
pleegwat <pleegwat@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-04-21 18:36:54 +0000

    Roster1 Trunk - SortMember - I'm a big believer in better safe than sorry. Extra access checks for the addon. AM needs these more than sortmember, but I like to keep a single line.

--- addons/SortMember/statslist.php
+++ addons/SortMember/statslist.php
@@ -21,6 +21,8 @@ if ( !defined('ROSTER_INSTALLED') )
     exit('Detected invalid access to this file!');
 }
 
+define('IN_SORTMEMBER',true);
+
 //---[ Check for Guild Info ]------------
 if( empty($guild_info) )
 {
