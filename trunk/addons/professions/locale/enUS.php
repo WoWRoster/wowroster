c92e1be5bc6c5caac99a1ef3c6d8c87d1f3e3581
Zanix <Zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-05-21 21:06:25 +0000

    Roster 1x Trunk: - Moved tradeskills to professions -- Added tooltip to button

--- trunk/addons/professions/locale/enUS.php
+++ trunk/addons/professions/locale/enUS.php
@@ -13,7 +13,9 @@
  * @subpackage Locale
 */
 
-$lang['admin']['tradeskill_conf'] = 'Professions|Control professions config options';
+$lang['professions_menu'] = 'Professions|Display a List of professions for all members';
+
+$lang['admin']['professions_conf'] = 'Professions|Control professions config options';
 
 $lang['admin']['show_new_skills'] = 'Show New Skills|This enables display of skills with a level of 1';
 $lang['admin']['collapse_list'] = 'Tradskill List Display|Controls how the tradskill lists display on page load <br /> The lists can be collapsed and opened by clicking on the header <br /><br /> &quot;Show&quot; will fully display the lists when the page loads <br /> &quot;Hide&quot; will show the lists collapsed';
