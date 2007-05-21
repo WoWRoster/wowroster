656142a12cb8328ca977d7ff30f8ccd1ae8080d3
Zanix <Zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-05-21 23:24:16 +0000

    again...

--- trunk/addons/keys/install.def.php
+++ trunk/addons/keys/install.def.php
@@ -58,7 +58,7 @@ class keys
 		$installer->add_config("'1020','colorcur','#ffd700','color','keys_conf'");
 		$installer->add_config("'1030','colorno','#ff0000','color','keys_conf'");
 
-		$installer->add_menu_button('keys','guild','');
+		$installer->add_menu_button('keys','guild');
 		return true;
 	}
 
