08f7b8e1c63a5dcb20606d5325f8ff60581a6207
Joshua <c.treyce@gmail.com>
2007-05-21 23:21:40 +0000

    WTF

--- addons/keys/install.def.php
+++ addons/keys/install.def.php
@@ -7,7 +7,7 @@
  *
  * @copyright  2002-2007 WoWRoster.net
  * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
- * @version    SVN: $Id:$
+ * @version    SVN: $Id: $
  * @link       http://www.wowroster.net
  * @package    InstanceKeys
  * @subpackage Installer
