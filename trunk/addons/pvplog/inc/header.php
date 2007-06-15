857bd8f8ca8c684a800e57485e804dcc585e85c5
Zanix <Zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-06-15 06:05:53 +0000

    Roster 1x trunk: - Small html fix for php errors in footer - $this->membersupdated, $this->membersadded, $this->membersremoved are now set to 0 after a guild is parsed - Fixed pvplog list to not error if the url param "start" < 0 - SEO set to 0 in upgrade_173.sql

--- trunk/addons/pvplog/inc/header.php
+++ trunk/addons/pvplog/inc/header.php
@@ -22,7 +22,7 @@ if( !defined('ROSTER_INSTALLED') )
 include_once($addon['dir'] . 'inc' . DIR_SEP . 'pvp.lib.php');
 
 // Check for start for pvp log data
-$start = (isset($_GET['start']) ? $_GET['start'] : 0);
+$start = (isset($_GET['start']) ? ( $_GET['start'] > 0 ? $_GET['start'] : 0 ) : 0);
 
 // Get pvp table/recipe sort mode
 $sort = (isset($_GET['s']) ? $_GET['s'] : '');
