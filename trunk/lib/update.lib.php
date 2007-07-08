b038d3b89f800d8c2249d78f461160e5dbc01f56
zeryl <zeryl@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-07-08 03:53:13 +0000

    Roster 1x trunk: Reverted last change, overlib doesn't translate htmlentities properly

--- trunk/lib/update.lib.php
+++ trunk/lib/update.lib.php
@@ -2571,12 +2571,6 @@ class update
 		$this->add_value('region', $region);
 		$this->add_ifvalue( $char, 'Class', 'class' );
 		$this->add_ifvalue( $char, 'Level', 'level' );
-		
-		if( isset($char['Note'] ))
-		{
-			$char['Note'] = htmlentities( $char['Note'] );
-		}
-		
 		$this->add_ifvalue( $char, 'Note', 'note', '' );
 		$this->add_ifvalue( $char, 'Rank', 'guild_rank');
 		$this->add_value('guild_title', $guildRanks[$char['Rank']]['Title']);
