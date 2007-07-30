e21aeb7ffda7054364af978aa204c612f75cceb3
Joshua <c.treyce@gmail.com>
2007-07-30 14:45:21 +0000

    Roster 1x trunk: - <PleegWat> Zanix, did you forget to add some files? -- Yes, I did

---
+++ addons/news/edit.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * WoWRoster.net WoWRoster
+ *
+ * LICENSE: Licensed under the Creative Commons
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * @copyright  2002-2007 WoWRoster.net
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
+ * @version    SVN: $Id$
+ * @link       http://www.wowroster.net
+ * @package    News
+*/
+
+include( $addon['dir'] . 'template' . DIR_SEP . 'template.php' );
+
+// Display news
+$query = "SELECT * "
+		. "FROM `" . $roster->db->table('news','news') . "` news "
+		. "WHERE `news_id` = '" . $_GET['id'] . "';";
+
+$result = $roster->db->query($query);
+
+if( $roster->db->num_rows($result) == 0 )
+{
+	echo messagebox($roster->locale->act['no_news']);
+}
+
+include_template( 'news_head.tpl' );
+
+$news = $roster->db->fetch($result);
+
+include_template( 'edit.tpl', $news );
