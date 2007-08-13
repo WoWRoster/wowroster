0a1050b00405485b3e7cc2fb1a77e7fba7503200
pleegwat <pleegwat@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-08-13 17:55:10 +0000

    Roster1 Trunk: - Changed the default default page to rostercp (from guild-memberslist)

--- lib/dbal/structure/mysql_data.sql
+++ lib/dbal/structure/mysql_data.sql
@@ -32,7 +32,7 @@ INSERT INTO `renprefix_config` VALUES (1002, 'sql_window', '1', 'radio{on^1|off^
 INSERT INTO `renprefix_config` VALUES (1010, 'minCPver', '2.1.1', 'text{10|10', 'main_conf');
 INSERT INTO `renprefix_config` VALUES (1020, 'minGPver', '2.1.0', 'text{10|10', 'main_conf');
 INSERT INTO `renprefix_config` VALUES (1040, 'locale', 'enUS', 'function{rosterLangValue', 'main_conf');
-INSERT INTO `renprefix_config` VALUES (1050, 'default_page', 'guild-memberslist', 'function{pageNames', 'main_conf');
+INSERT INTO `renprefix_config` VALUES (1050, 'default_page', 'rostercp', 'function{pageNames', 'main_conf');
 INSERT INTO `renprefix_config` VALUES (1060, 'website_address', '', 'text{128|30', 'main_conf');
 INSERT INTO `renprefix_config` VALUES (1085, 'interface_url', 'img/', 'text{128|30', 'main_conf');
 INSERT INTO `renprefix_config` VALUES (1090, 'img_suffix', 'jpg', 'select{jpg^jpg|png^png|gif^gif', 'main_conf');
