2c6ee795f21254da1532556b77535ccf44df5140
Joshua <c.treyce@gmail.com>
2007-08-16 04:19:50 +0000

    Roster 1.9.9: - Added version tags

--- pages/search.php
+++ pages/search.php
@@ -9,7 +9,7 @@
  *
  * @copyright  2002-2007 WoWRoster.net
  * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
- * @version
+ * @version    SVN: $Id$
  * @link       http://www.wowroster.net
  * @package    WoWRoster
 */
