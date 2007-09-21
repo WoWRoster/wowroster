df5f62286221e60455cccba050b944cff0fb4f46
Joshua <c.treyce@gmail.com>
2007-09-21 01:49:33 +0000

    Roster 1.9.9 beta: - Updated esES Locale -- Thanks maqjav

---
+++ addons/professions/locale/esES.php
@@ -0,0 +1,23 @@
+﻿<?php
+/**
+ * WoWRoster.net WoWRoster
+ *
+ * LICENSE: Licensed under the Creative Commons
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ * @copyright  2002-2007 WoWRoster.net
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
+ * @version    SVN: $Id$
+ * @link       http://www.wowroster.net
+ * @package    Professions
+ * @subpackage Locale
+*/
+
+$lang['professions_desc'] = 'Listado con las profesiones de todos los miembros de la hermandad';
+
+$lang['professions_menu'] = 'Profesiones|Muestra un listado con las profesiones de todos los miembros de la hermandad';
+
+$lang['admin']['professions_conf'] = 'Profesiones|Configura las opciones de las profesiones';
+
+$lang['admin']['show_new_skills'] = 'Mostrar nuevas habilidades|Permite mostrar habilidades con nivel 1';
+$lang['admin']['collapse_list'] = 'Mostrar listado de habilidades|Configura como mostrar la lista con todas las habilidades al cargar la página <br /> Las listas se pueden cerrar y abrir haciendo click en la cabecera <br /><br /> &quot;Mostrar&quot; mostrará por completo las listas cuando se carga la página <br /> &quot;Esconder&quot; mostrará las listas cerradas';
--- lib/constants.php
+++ lib/constants.php
@@ -20,7 +20,7 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION','1.9.9.1337');
+define('ROSTER_VERSION','1.9.9.1347');
 
 /**
  * Roster Conf File
