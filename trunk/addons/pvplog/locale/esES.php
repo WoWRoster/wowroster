b5fa378de968f34417a710d619f70b7ea28ca8b8
Zanix <Zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-09-21 01:49:33 +0000

    Roster 1.9.9 beta: - Updated esES Locale -- Thanks maqjav

--- trunk/addons/pvplog/locale/esES.php
+++ trunk/addons/pvplog/locale/esES.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * WoWRoster.net WoWRoster
  *
@@ -13,35 +13,35 @@
  * @subpackage Locale
 */
 
-$lang['pvplog_desc'] = 'Displays data collected by the PvPLog WoW addon';
+$lang['pvplog_desc'] = 'Muestra datos capturados con el addon PvPLog';
 
-$lang['button_pvplog']='Estadísticas JcJ/PvP|Shows PvP Log stats for this guild';
-$lang['button_bg']='Battleground Log|Battleground stats from PvP Log';
-$lang['button_pvp']='PvP Log|World PvP stats from PvP Log';
-$lang['button_duel']='Duel Log|Duel stats from PvP Log';
+$lang['button_pvplog']='Estadísticas JcJ|Muestra un registro con las estadísticas de JcJ en la hermandad';
+$lang['button_bg']='Registro de CDB|Muestra las estadísticas en campos de batalla del PvP Log';
+$lang['button_pvp']='PvP Log|Muestra las estadísticas de JcJ en todo el mundo del PvP Log';
+$lang['button_duel']='Registro de duelos|Muestra las estadísticas en los duelos del PvP Log';
 
-$lang['bglog']='Registro Batallas';
+$lang['bglog']='Registro de batallas';
 $lang['pvplog']='Registro JcJ';
-$lang['duellog']='Registro Duelos';
-$lang['duelsummary']='Resumen Duelos';
+$lang['duellog']='Registro de duelos';
+$lang['duelsummary']='Resumen de duelos';
 
-$lang['totalwins']='Victorias Totales';
-$lang['totallosses']='Derrotas Totales';
+$lang['totalwins']='Victorias totales';
+$lang['totallosses']='Derrotas totales';
 $lang['totaloverall']='Total';
 $lang['win_average']='Promedio de diferencias de nivel (Victorias)';
 $lang['loss_average']='Promedio de diferencias de nivel (Derrotas)';
 
 // These need to be EXACTLY what PvPLog stores them as
-$lang['eye_of_the_storm']='Eye of the Storm';
+$lang['eye_of_the_storm']='Ojo de la Tormenta';
 $lang['alterac_valley']='Valle de Alterac';
 $lang['arathi_basin']='Cuenca de Arathi';
 $lang['warsong_gulch']='Garganta Grito de Guerra';
 
-$lang['world_pvp']='JcJ Mundial';
-$lang['versus_guilds']='Contra Hermandades';
-$lang['versus_players']='Contra Jugadores';
-$lang['bestsub']='Mejor Subzona';
-$lang['worstsub']='Peor Subzona';
+$lang['world_pvp']='JcJ mundial';
+$lang['versus_guilds']='Contra hermandades';
+$lang['versus_players']='Contra jugadores';
+$lang['bestsub']='Mejor subzona';
+$lang['worstsub']='Peor subzona';
 $lang['killedmost']='El más asesinado';
 $lang['killedmostby']='El que más nos ha asesinado';
 $lang['gkilledmost']='La hermandad más asesinada';
@@ -50,30 +50,30 @@ $lang['gkilledmostby']='El que más ha asesinado a nuestra hermandad';
 $lang['wins']='Victorias';
 $lang['losses']='Derrotas';
 $lang['overall']='Total';
-$lang['best_zone']='Mejor Zona';
-$lang['worst_zone']='Peor Zona';
+$lang['best_zone']='Mejor zona';
+$lang['worst_zone']='Peor zona';
 $lang['most_killed']='El más asesinado';
 $lang['most_killed_by']='El que más nos ha asesinado';
-$lang['leveldiff']='Dif Nivel';
-$lang['bg']='Campo de Batalla';
+$lang['leveldiff']='Dif. de nivel';
+$lang['bg']='Campo de batalla';
 
 // guildpvp strings
-$lang['guildwins'] = 'Wins by Guild';
-$lang['guildlosses'] = 'Losses by Guild';
-$lang['enemywins'] = 'Wins by Enemy';
-$lang['enemylosses'] = 'Losses by Enemy';
-$lang['purgewins'] = 'Guild Member Kills';
-$lang['purgelosses'] = 'Guild Member Deaths';
-$lang['purgeavewins'] = 'Best Win/Level-Diff Average';
-$lang['purgeavelosses'] = 'Best Loss/Level-Diff Average';
-$lang['pvpratio'] = 'Solo Win/Loss Ratios';
-$lang['playerinfo'] = 'Player Info';
-$lang['kill_lost_hist']='Kill/Loss history for %1$s (%2$s %3$s) of %4$s';
-$lang['kill_lost_hist_guild'] = 'Kill/Loss history for Guild &quot;%1$s&quot;';
-$lang['solo_win_loss'] = 'Solo Win/Loss Ratios (Level differences -7 to +7 counted)';
-$lang['guildinfo'] = 'Guild Info';
+$lang['guildwins'] = 'Victorias por hermandad';
+$lang['guildlosses'] = 'Derrotas por hermandad';
+$lang['enemywins'] = 'Victorias por enemigo';
+$lang['enemylosses'] = 'Derrotas por enemigo';
+$lang['purgewins'] = 'Asesinatos de miembros de la hermandad';
+$lang['purgelosses'] = 'Muertes de miembros de la hermandad';
+$lang['purgeavewins'] = 'Mejor media de victoria/dif. de nivel';
+$lang['purgeavelosses'] = 'Mejor media de derrota/dif. de nivel';
+$lang['pvpratio'] = 'Media de victorias/derrotas en solitario';
+$lang['playerinfo'] = 'Información del jugador';
+$lang['kill_lost_hist']='Historial de asesinatos/muertes de %1$s (%2$s %3$s) de %4$s';
+$lang['kill_lost_hist_guild'] = 'Historial de asesinatos/muertes de la hermandad &quot;%1$s&quot;';
+$lang['solo_win_loss'] = 'Media de victorias/derrotas en solitario (Diferencia de nivel -7 a +7 contado)';
+$lang['guildinfo'] = 'Información de la hermandad';
 
 $lang['PvPLogver_err']='La versión de PvPLog uutilizada para capturar los datos de este personaje es inferior a la versión mínima admitida para el envío.<br />Por favor, asegúrate de que estás utilizando al menos la versión %1$s, y si has actualizado recientemente PvPLog, asegúrate de que has borrado el antiguo fichero PvPLog.lua en la carpeta SavedVariables antes de enviar los datos.';
 
-$lang['admin']['pvpconfig'] = "PvPLog Config|Configure PvPLog main options";
-$lang['admin']['minPvPLogver'] = "Min PvPLog versi&oacuten|M&iacutenima versi&oacuten permitida para usar el PvPLog";
+$lang['admin']['pvpconfig'] = "Configuración de PvPLog|Configura las opciones de PvPLog";
+$lang['admin']['minPvPLogver'] = "Min. versión de PvPLog|Mínima versión permitida de PvPLog";
