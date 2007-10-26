2d6faca34853713f9b0dae1fd05df7217e95e8d4
Zanix <Zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-10-26 17:19:44 +0000

    Roster 1.9.9: - Fixed memberlog not getting all the data on member deletion -- http://www.wowroster.net/Forums/viewtopic/t=4440.html

--- trunk/lib/update.lib.php
+++ trunk/lib/update.lib.php
@@ -2343,7 +2343,7 @@ class update
 	{
 		global $roster;
 
-		$querystr = "SELECT `member_id`, `name` FROM `" . $roster->db->table('members') . "` WHERE `guild_id` = '$guild_id' AND `active` = '0';";
+		$querystr = "SELECT * FROM `" . $roster->db->table('members') . "` WHERE `guild_id` = '$guild_id' AND `active` = '0';";
 
 		$result = $roster->db->query($querystr);
 		if( !$result )
