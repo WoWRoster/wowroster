306ae374a3ef0ea928d047d04b9d3fe18d6f6ae3
Joshua <c.treyce@gmail.com>
2007-11-01 17:48:44 +0000

    Roster 1.9.9 beta: - One last notice error -- http://www.wowroster.net/Forums/viewtopic/t=4571.html

--- lib/constants.php
+++ lib/constants.php
@@ -20,7 +20,7 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION','1.9.9.1423');
+define('ROSTER_VERSION','1.9.9.1424');
 
 /**
  * Roster Conf File
