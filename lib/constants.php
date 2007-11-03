83b2e24d6df6ed70b65f2fe3cfe0c8f77b68161d
pleegwat <pleegwat@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-11-03 17:33:29 +0000

    Made an error changing version.txt

--- lib/constants.php
+++ lib/constants.php
@@ -20,7 +20,7 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION','1.9.9.1426');
+define('ROSTER_VERSION','1.9.9.1427');
 
 /**
  * Roster Conf File
