01520bcca33575ca351da3be87f3d40aaa13cb0f
pleegwat <pleegwat@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-11-05 09:24:11 +0000

    Roster1 trunk: Update - Added guild deletion routines and delete-by-rules routine

--- lib/constants.php
+++ lib/constants.php
@@ -20,7 +20,7 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION','1.9.9.1428');
+define('ROSTER_VERSION','1.9.9.1429');
 
 /**
  * Roster Conf File
