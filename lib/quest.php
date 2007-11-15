dfc3bad79a373130c1a92ecdf9e734a37d3fc4c5
AnthonyB <AnthonyB@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-11-15 09:22:41 +0000

    Roster 1.9.9: - Made quests sort in ASC order by level within zone like in-game rather than DESC.

--- lib/quest.php
+++ lib/quest.php
@@ -82,7 +82,7 @@ function quest_get_many( $member_id, $search )
 {
 	global $roster;
 
-	$query= "SELECT * FROM `".$roster->db->table('quests')."` WHERE `member_id` = '$member_id' ORDER BY `zone` ASC, `quest_level` DESC";
+	$query= "SELECT * FROM `".$roster->db->table('quests')."` WHERE `member_id` = '$member_id' ORDER BY `zone` ASC, `quest_level` ASC";
 
 	$result = $roster->db->query( $query );
 
