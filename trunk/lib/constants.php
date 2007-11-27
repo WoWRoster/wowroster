831d611e190f242b4f772a91988aeca3ad65a5ae
Zanix <Zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-11-27 05:39:36 +0000

    Roster 1.9.9: - Oops, didn't turn off all instances of getting items in vault

--- trunk/lib/constants.php
+++ trunk/lib/constants.php
@@ -20,7 +20,7 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION','1.9.9.1484');
+define('ROSTER_VERSION','1.9.9.1485');
 
 /**
  * Roster Conf File
