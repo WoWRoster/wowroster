fbfc1edbba4581b97324e61bd1a9bd1a6246e2a2
Zanix <Zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2007-11-29 04:16:07 +0000

    Roster 1.9.9: - Fixed typo in char-info admin page - Added partial frFR locale to vault addon -- Thanks tuigii - Added external auth system as requested by boyo -- http://www.wowroster.net/Forums/viewtopic/t=4684.html -- Any addon with the file inc/login.php will be selectable in RosterCP->Main Settings and that file will take over the auth system in Roster - Removed SEO check in header template file, its safe to have baseurl in there

---
+++ trunk/addons/vault/locale/frFR.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * WoWRoster.net WoWRoster
+ *
+ * LICENSE: Licensed under the Creative Commons
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ *
+ *
+ * @copyright  2002-2007 WoWRoster.net
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
+ * @version    SVN: $Id$
+ * @link       http://www.wowroster.net
+ * @package    Vault
+ * @subpackage Locale
+*/
+
+$lang['vault']='Guild Vault';
+$lang['vault_desc']='Montre la continue du Vault de la Guilde';
+$lang['vault_menu']='Guild Vault|Montre la continue du Vault de la Guilde';
+$lang['available_amount']='Montant disponible';
+$lang['vault_log']='Trace';
+$lang['vault_money_log']="Trace de l’argent";
+
+$lang['vault_log_deposit'] = '%1$s <span class="greenB">a déposé</span> %2$s';
+$lang['vault_log_withdraw'] = '%1$s <span class="redB">a rétiré</span> %2$s';
+$lang['vault_log_move'] = '%1$s <span class="purpleB">a dépacé</span> %2$s';
+
+$lang['admin']['vault_conf'] = 'Guild Vault|Set up vault display and settings';
+$lang['admin']['tab1'] = 'Tab 1 Access|Control who can view the items in Tab 1';
+$lang['admin']['tab2'] = 'Tab 2 Access|Control who can view the items in Tab 2';
+$lang['admin']['tab3'] = 'Tab 3 Access|Control who can view the items in Tab 3';
+$lang['admin']['tab4'] = 'Tab 4 Access|Control who can view the items in Tab 4';
+$lang['admin']['tab5'] = 'Tab 5 Access|Control who can view the items in Tab 5';
+$lang['admin']['money'] = 'Money Access|Control who can view the available money';
