ea38d73be89a9bed1cc3decaeabc2e3314549963
Joshua <c.treyce@gmail.com>
2007-12-16 19:44:58 +0000

    Roster 1.9.9: - Updated version

--- lib/constants.php
+++ lib/constants.php
@@ -20,7 +20,7 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION','1.9.9.1538');
+define('ROSTER_VERSION','1.9.9.1540');
 
 /**
  * Roster Conf File
