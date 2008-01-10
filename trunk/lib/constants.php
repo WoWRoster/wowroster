423c723f7ce3f8941c5deaa3b6562e0b4e3568c6
pleegwat <pleegwat@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2008-01-10 13:02:48 +0000

    Roster1 Trunk: - Upped the version number because I want to be able to see if people have that last commit

--- trunk/lib/constants.php
+++ trunk/lib/constants.php
@@ -20,7 +20,7 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION','1.9.9.1577');
+define('ROSTER_VERSION','1.9.9.1579');
 
 /**
  * Roster Conf File
