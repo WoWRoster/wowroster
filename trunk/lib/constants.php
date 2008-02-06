c4947eeeb190fe53c718dd3a61d1a3cbc7caed75
pleegwat <pleegwat@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2008-02-06 18:39:17 +0000

    Roster1 Trunk: 1.9.9.1634 - Updating version for beta push

--- trunk/lib/constants.php
+++ trunk/lib/constants.php
@@ -20,7 +20,7 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION','1.9.9.1632');
+define('ROSTER_VERSION','1.9.9.1634');
 
 /**
  * Roster Conf File
