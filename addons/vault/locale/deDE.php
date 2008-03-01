c5748684399377e161558990906249bbdbb26024
Joshua <c.treyce@gmail.com>
2008-03-01 03:34:40 +0000

    Roster trunk: - deDE updates by Holgiranemsi

---
+++ addons/vault/locale/deDE.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * WoWRoster.net WoWRoster
+ *
+ * LICENSE: Licensed under the Creative Commons
+ *          "Attribution-NonCommercial-ShareAlike 2.5" license
+ *
+ *
+ *
+ * @copyright  2002-2007 WoWRoster.net
+ * @license    http://creativecommons.org/licenses/by-nc-sa/2.5   Creative Commons "Attribution-NonCommercial-ShareAlike 2.5"
+ * @version    SVN: $Id$
+ * @link       http://www.wowroster.net
+ * @package    Vault
+ * @subpackage Locale
+*/
+
+$lang['vault']='Gildentresor';
+$lang['vault_desc']='Zeigt den Inhalt des Gildentresors';
+$lang['vault_menu']='Gildentresor|Zeigt den Inhalt des Gildentresors';
+$lang['available_amount']='Verfügbare Summe';
+$lang['vault_log']='Item Log';
+$lang['vault_money_log']='Geld Log';
+
+$lang['vault_log_deposit'] = '%1$s<span class="greenB">Einlagerung</span> %2$s';
+$lang['vault_log_withdraw'] = '%1$s <span class="redB">Entnahme</span> %2$s';
+$lang['vault_log_move'] = '%1$s <span class="purpleB">Verschiebung</span> %2$s';
+$lang['vault_log_repair'] = '%1$s <span class="redB">spent</span> %2$s for repairs';
+
+$lang['admin']['vault_conf'] = 'Gildentresor|Konfiguration der Anzeige';
+$lang['admin']['tab1'] = 'Fach 1 Zugriff|Regelt den Zugriff auf Fach 1';
+$lang['admin']['tab2'] = 'Fach 2 Zugriff|Regelt den Zugriff auf Fach 2';
+$lang['admin']['tab3'] = 'Fach 3 Zugriff|Regelt den Zugriff auf Fach 3';
+$lang['admin']['tab4'] = 'Fach 4 Zugriff|Regelt den Zugriff auf Fach 4';
+$lang['admin']['tab5'] = 'Fach 5 Zugriff|Regelt den Zugriff auf Fach 5';
+$lang['admin']['tab6'] = 'Fach 6 Zugriff|Regelt den Zugriff auf Fach 6';
+$lang['admin']['money'] = 'Gold Zugriff|Regelt den Zugriff auf das Gold';
