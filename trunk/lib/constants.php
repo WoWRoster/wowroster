9cce21140650b6e848a14a2d70a0915e073a425c
Zanix <Zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2008-06-20 00:54:55 +0000

    Roster Trunk: - Type cast passed variables for search to int - Added favicon.ico :D - frFR Updates - <pre> text in aprint() will now wrap to screen

--- trunk/lib/constants.php
+++ trunk/lib/constants.php
@@ -20,7 +20,7 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION','1.9.9.1794');
+define('ROSTER_VERSION','1.9.9.1797');
 
 /**
  * Roster Conf File
