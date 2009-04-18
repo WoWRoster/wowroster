425d9df797252da350c0b9a8097687520aa48319
zanix <zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2009-04-18 08:54:35 +0000

    Roster Trunk: I'm dry coding this, so i hope it doesn't break - Moved alot of the functionality from menu.php into header.php -- maketop and makebottom no longer exist - TPL vars have changed names, some new ones have been added as well - Renamed upgrade_201.sql to upgrade_202.sql

--- trunk/header.php
+++ trunk/header.php
@@ -43,21 +43,68 @@ switch( $roster->scope )
 	case 'page':
 		$roster_title = ' [ ' . $roster->config['default_name'] . ' ] '
 					  . (isset($roster->output['title']) ? $roster->output['title'] : '');
+
+		$roster->tpl->assign_vars(array(
+			'ROSTER_TITLE'    => $roster->config['default_name'],
+			'ROSTER_SUBTITLE' => isset($roster->config['default_desc']) ? '<br />' . $roster->config['default_desc'] : '',
+			'ROSTER_3RDTITLE' => false,
+			'LAST_UPDATED'    => false,
+			)
+		);
 		break;
 
 	case 'realm':
 		$roster_title = ' [ ' . $roster->data['region'] . '-' . $roster->data['server'] . ' ] '
 					  . (isset($roster->output['title']) ? $roster->output['title'] : '');
+
+		$roster->tpl->assign_vars(array(
+			'ROSTER_TITLE'    => $roster->data['region'] . '-' . $roster->data['server'],
+			'ROSTER_SUBTITLE' => isset($roster->config['default_desc']) && $roster->config['default_desc'] != ''
+										? $roster->config['default_desc'] : false,
+			'ROSTER_3RDTITLE' => false,
+			'LAST_UPDATED'    => false,
+			)
+		);
 		break;
 
 	case 'guild':
+		$roster_title = ' [ ' . $roster->data['guild_name'] . ' @ ' . $roster->data['region'] . '-' . $roster->data['server'] . ' ] '
+					  . (isset($roster->output['title']) ? $roster->output['title'] : '');
+
+		$roster->tpl->assign_vars(array(
+			'ROSTER_TITLE' => $roster->data['guild_name'],
+			'ROSTER_SUBTITLE' => '@ ' . $roster->data['region'] . '-' . $roster->data['server'],
+			'ROSTER_3RDTITLE' => $roster->data['guild_num_members'] . $roster->locale->act['members'],
+			'LAST_UPDATED'    => ( isset($roster->data['update_time']) ? readbleDate($roster->data['update_time'])
+					. ( (!empty($roster->config['timezone'])) ? ' (' . $roster->config['timezone'] . ')' : '') : '' ),
+			)
+		);
+		break;
+
 	case 'char':
 		$roster_title = ' [ ' . $roster->data['guild_name'] . ' @ ' . $roster->data['region'] . '-' . $roster->data['server'] . ' ] '
 					  . (isset($roster->output['title']) ? $roster->output['title'] : '');
+
+		$roster->tpl->assign_vars(array(
+			'ROSTER_TITLE' => $roster->data['name'],
+			'ROSTER_SUBTITLE' => '@ ' . $roster->data['region'] . '-' . $roster->data['server'],
+			'ROSTER_3RDTITLE' => false,
+			'LAST_UPDATED'    => ( isset($roster->data['update_time']) ? readbleDate($roster->data['update_time'])
+					. ( (!empty($roster->config['timezone'])) ? ' (' . $roster->config['timezone'] . ')' : '') : '' ),
+			)
+		);
 		break;
 
 	default:
 		$roster_title = (isset($roster->output['title']) ? $roster->output['title'] : '');
+
+		$roster->tpl->assign_vars(array(
+			'ROSTER_TITLE' => (isset($roster->output['title']) ? $roster->output['title'] : ''),
+			'ROSTER_SUBTITLE' => false,
+			'ROSTER_3RDTITLE' => false,
+			'LAST_UPDATED'    => false,
+			)
+		);
 		break;
 }
 
@@ -76,16 +123,180 @@ $roster->tpl->assign_vars(array(
 	'HEADER_LOGO'     => $roster->config['logo'],
 	'IMG_URL'         => $roster->config['img_url'],
 	// End duplication
-	'ROSTER_SCOPE'  => $roster->scope,
+	'ROSTER_SCOPE'    => $roster->scope,
 
 	'PAGE_TITLE'      => $roster_title,
 	'ROSTER_HEAD'     => $roster->output['html_head'],
 	'ROSTER_BODY'     => (!empty($roster->config['roster_bg']) ? ' style="background-image:url(' . $roster->config['roster_bg'] . ');"' : '')
 					   . (!empty($roster->output['body_attr']) ? ' ' . $roster->output['body_attr'] : ''),
 	'ROSTER_ONLOAD'   => (!empty($roster->output['body_onload']) ? $roster->output['body_onload'] : ''),
-	'ROSTER_MENU_BEFORE' => $roster->output['before_menu'],
+
+	'ROSTER_MENU_BEFORE'   => $roster->output['before_menu'],
+	'MENU_LOGIN_FORM'      => ( is_object($roster->auth) ? $roster->auth->getMenuLoginForm() : '' ),
+	'U_SEARCH_FORM_ACTION' => makelink('search'),
+
+	'S_LOCALE_SELECT'   => (bool)$roster->config['menu_top_locale'],
+
+	'L_MENU_LABEL'      => $roster->scope,
+	'L_MENU_LABEL_NAME' => $roster->locale->act[$roster->scope],
+
+	'FACTION' => isset($roster->data['factionEn']) ? strtolower($roster->data['factionEn']) : false,
 	)
 );
+
+// Make a listing of our current locales
+if( $roster->config['menu_top_locale'] )
+{
+	foreach( $roster->multilanguages as $language )
+	{
+		$roster->tpl->assign_block_vars('locale_select', array(
+			'LOCALE'      => $language,
+			'LOCALE_NAME' => $roster->locale->wordings[$language]['langname'],
+			'S_SELECTED'  => ( $language == $roster->config['locale'] ? true : false ),
+			)
+		);
+	}
+}
+
+/**
+ * Make the data selection list
+ */
+$menu_select = array();
+$roster->tpl->assign_var('S_DATA_SELECT',false);
+
+if( $roster->scope == 'realm' )
+{
+       // Get the scope select data
+       $query = "SELECT DISTINCT `server`, `region`"
+              . " FROM `" . $roster->db->table('guild') . "`"
+              . " UNION SELECT DISTINCT `server`, `region` FROM `" . $roster->db->table('players') . "`"
+              . " ORDER BY `server` ASC;";
+
+       $result = $roster->db->query($query);
+
+       if( !$result )
+       {
+           die_quietly($roster->db->error(),'Database error',__FILE__,__LINE__,$query);
+       }
+
+       $realms=0;
+	while( $data = $roster->db->fetch($result,SQL_NUM) )
+	{
+		$menu_select[$data[1]][] = $data[0];
+		$realms++;
+	}
+
+       $roster->db->free_result($result);
+
+       $roster->tpl->assign_var('S_DATA_SELECT',( $realms > 1 ? true : false ));
+
+	if( $realms > 1 )
+	{
+		foreach( $menu_select as $region => $realmsArray )
+		{
+			$roster->tpl->assign_block_vars('menu_select_group', array(
+				'U_VALUE'      => $region,
+				)
+			);
+
+			foreach( $realmsArray as $name )
+			{
+				$roster->tpl->assign_block_vars('menu_select_group.menu_select_row', array(
+					'TEXT'       => $name,
+					'U_VALUE'    => makelink("&amp;a=r:$region-$name",true),
+					'S_SELECTED' => ( $name == $roster->data['server'] ? true : false )
+					)
+				);
+			}
+		}
+	}
+}
+elseif( $roster->scope == 'guild' )
+{
+	// Get the scope select data
+	$query = "SELECT `guild_name`, CONCAT(`region`,'-',`server`), `guild_id` FROM `" . $roster->db->table('guild') . "`"
+		   . " ORDER BY `region` ASC, `server` ASC, `guild_name` ASC;";
+
+	$result = $roster->db->query($query);
+
+	if( !$result )
+	{
+		die_quietly($roster->db->error(),'Database error',__FILE__,__LINE__,$query);
+	}
+
+       $guilds=0;
+	while( $data = $roster->db->fetch($result,SQL_NUM) )
+	{
+		$menu_select[$data[1]][$data[2]] = $data[0];
+		$guilds++;
+	}
+
+	$roster->db->free_result($result);
+
+       $roster->tpl->assign_var('S_DATA_SELECT',( $guilds > 1 ? true : false ));
+
+	if( count($menu_select) > 0 )
+	{
+		foreach( $menu_select as $realm => $guild )
+		{
+			$roster->tpl->assign_block_vars('menu_select_group', array(
+				'U_VALUE'      => $realm,
+				)
+			);
+
+			foreach( $guild as $id => $name )
+			{
+				$roster->tpl->assign_block_vars('menu_select_group.menu_select_row', array(
+					'TEXT'       => $name,
+					'U_VALUE'    => makelink('&amp;a=g:' . $id,true),
+					'S_SELECTED' => ( $id == $roster->data['guild_id'] ? true : false )
+					)
+				);
+			}
+		}
+	}
+}
+elseif( $roster->scope == 'char' )
+{
+	// Get the scope select data
+	$query = "SELECT `name`, `member_id` FROM `" . $roster->db->table('players') . "`"
+		   . " WHERE `guild_id` = '" . $roster->data['guild_id'] . "'"
+		   . " ORDER BY `name` ASC;";
+
+	$result = $roster->db->query($query);
+
+	if( !$result )
+	{
+		die_quietly($roster->db->error(),'Database error',__FILE__,__LINE__,$query);
+	}
+
+	while( $data = $roster->db->fetch($result,SQL_NUM) )
+	{
+		$menu_select[$data[1]] = $data[0];
+	}
+
+       $roster->tpl->assign_var('S_DATA_SELECT',( $roster->db->num_rows() > 1 ? true : false ));
+
+	$roster->db->free_result($result);
+
+	if( count($menu_select) > 0 )
+	{
+		$roster->tpl->assign_block_vars('menu_select_group', array(
+			'U_VALUE'      => $roster->data['guild_name'],
+			)
+		);
+
+		foreach( $menu_select as $id => $name )
+		{
+			$roster->tpl->assign_block_vars('menu_select_group.menu_select_row', array(
+				'TEXT'       => $name,
+				'U_VALUE'    => makelink('&amp;a=c:' . $id,true),
+				'S_SELECTED' => ( $id == $roster->data['member_id'] ? true : false )
+				)
+			);
+		}
+	}
+}
 
 
 $roster->tpl->set_handle('roster_header', 'header.html');
---
+++ trunk/lib/dbal/structure/upgrade_202.sql
@@ -0,0 +1,110 @@
+#
+# MySQL WoWRoster Upgrade File
+#
+# * $Id$
+#
+# --------------------------------------------------------
+### Glyphs
+
+DROP TABLE IF EXISTS `renprefix_glyphs`;
+CREATE TABLE `renprefix_glyphs` (
+  `member_id` int(11) unsigned NOT NULL default '0',
+  `glyph_order` tinyint(4) NOT NULL default '0',
+  `glyph_type` tinyint(4) NOT NULL default '0',
+  `glyph_name` varchar(96) NOT NULL default '',
+  `glyph_icon` varchar(64) NOT NULL default '',
+  `glyph_tooltip` mediumtext NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+# --------------------------------------------------------
+### Pets
+
+ALTER TABLE `renprefix_pets`
+  DROP `usedtp`,
+  DROP `loyalty`;
+
+# --------------------------------------------------------
+### Pet Spellbook
+
+CREATE TABLE `renprefix_pet_spellbook` (
+  `member_id` int( 11 ) unsigned NOT NULL default '0',
+  `pet_id` int( 11 ) unsigned NOT NULL default '0',
+  `spell_name` varchar( 64 ) NOT NULL default '',
+  `spell_texture` varchar( 64 ) NOT NULL default '',
+  `spell_rank` varchar( 64 ) NOT NULL default '',
+  `spell_tooltip` mediumtext NOT NULL ,
+  PRIMARY KEY ( `member_id` , `pet_id` , `spell_name` , `spell_rank` )
+) ENGINE = MYISAM DEFAULT CHARSET = utf8;
+
+INSERT INTO `renprefix_pet_spellbook`
+  SELECT *
+  FROM `renprefix_spellbook_pet` ;
+
+DROP TABLE `renprefix_spellbook_pet` ;
+
+# --------------------------------------------------------
+### Pet Talents
+
+DROP TABLE IF EXISTS `renprefix_pet_talents`;
+CREATE TABLE `renprefix_pet_talents` (
+  `member_id` int(11) NOT NULL default '0',
+  `pet_id` int(11) unsigned NOT NULL default '0',
+  `name` varchar(64) NOT NULL default '',
+  `tree` varchar(64) NOT NULL default '',
+  `row` tinyint(4) NOT NULL default '0',
+  `column` tinyint(4) NOT NULL default '0',
+  `rank` tinyint(4) NOT NULL default '0',
+  `maxrank` tinyint(4) NOT NULL default '0',
+  `tooltip` mediumtext NOT NULL,
+  `icon` varchar(64) NOT NULL default '',
+  PRIMARY KEY  (`member_id`,`pet_id`,`tree`,`row`,`column`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+# --------------------------------------------------------
+### Pet Talent Tree
+
+DROP TABLE IF EXISTS `renprefix_pet_talenttree`;
+CREATE TABLE `renprefix_pet_talenttree` (
+  `member_id` int(11) NOT NULL default '0',
+  `pet_id` int(11) unsigned NOT NULL default '0',
+  `tree` varchar(64) NOT NULL default '',
+  `background` varchar(64) NOT NULL default '',
+  `order` tinyint(4) NOT NULL default '0',
+  `pointsspent` tinyint(4) NOT NULL default '0',
+  PRIMARY KEY  (`member_id`,`pet_id`,`tree`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+# --------------------------------------------------------
+### Quest Data
+
+DROP TABLE IF EXISTS `renprefix_quest_data`;
+CREATE TABLE `renprefix_quest_data` (
+  `quest_id` int(11) NOT NULL default '0',
+  `quest_name` varchar(64) NOT NULL default '',
+  `quest_level` int(11) unsigned NOT NULL default '0',
+  `quest_tag` varchar(32) NOT NULL default '',
+  `group` int(1) NOT NULL default '0',
+  `daily` int(1) NOT NULL default '0',
+  `reward_money` int(11) NOT NULL default '0',
+  `zone` varchar(32) NOT NULL default '',
+  `description` text NOT NULL,
+  `objective` text NOT NULL,
+  `locale` varchar(4) NOT NULL default '',
+  PRIMARY KEY  (`quest_id`,`locale`),
+  FULLTEXT KEY `quest_name` (`quest_name`),
+  FULLTEXT KEY `zone` (`zone`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+# --------------------------------------------------------
+### Quests
+
+DROP TABLE IF EXISTS `renprefix_quests`;
+CREATE TABLE `renprefix_quests` (
+  `member_id` int(11) unsigned NOT NULL default '0',
+  `quest_id` int(11) NOT NULL default '0',
+  `quest_index` int(11) NOT NULL default '0',
+  `difficulty` int(1) NOT NULL default '0',
+  `is_complete` int(1) NOT NULL default '0',
+  PRIMARY KEY  (`member_id`,`quest_id`),
+  KEY `quest_index` (`quest_id`,`quest_index`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
