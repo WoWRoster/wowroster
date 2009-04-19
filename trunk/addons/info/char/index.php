848fe6a498cfeaf2a8aaf7c8bb681d17891d0777
zanix <zanix@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2009-04-19 22:45:07 +0000

    Roster Trunk: - Typos?

--- trunk/addons/info/char/index.php
+++ trunk/addons/info/char/index.php
@@ -19,14 +19,6 @@ if( !defined('IN_ROSTER') )
     exit('Detected invalid access to this file!');
 }
 
-/*
-if( $roster->auth->getAuthorized($addon['config']['show_item_bonuses']) )
-{
-	$roster->output['html_head'] .= '<script type="text/javascript" src="' . $addon['url_path'] . 'js/overlib_overtwo.js"></script>';
-	$roster->output['html_head'] .= '<script type="text/javascript" src="' . ROSTER_PATH . 'js/overlib.js"></script>';
-}
-*/
-
 include( $addon['inc_dir'] . 'header.php' );
 
 print $char->out();
