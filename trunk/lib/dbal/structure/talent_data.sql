df5dd148e253c4e5d58bd2a85e937ac939a7b00b
c.treyce@gmail.com <c.treyce@gmail.com@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2010-12-05 10:28:06 +0000

    Roster Trunk: - Ulminia removed expertise updating, so the db fields have been removed

--- trunk/lib/dbal/structure/talent_data.sql
+++ trunk/lib/dbal/structure/talent_data.sql
@@ -1,23 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 3.3.3
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Nov 30, 2010 at 06:40 AM
--- Server version: 5.1.36
--- PHP Version: 5.3.0
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `wowroster`
---
+#
+# MySQL Talent Data File
+#
+# * $Id$
+#
+# --------------------------------------------------------
+### Account
 
 --
 -- Dumping data for table `roster_talents_data`
