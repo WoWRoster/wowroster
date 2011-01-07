21c73126123728c8e09162787e9d2957808e05f0
c.treyce@gmail.com <c.treyce@gmail.com@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2011-01-07 07:07:06 +0000

    Roster Trunk: 2.0.9.2249 - Updating version information for Beta Push

--- trunk/lib/constants.php
+++ trunk/lib/constants.php
@@ -18,7 +18,7 @@ if( !defined('IN_ROSTER') )
 	exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION', '2.0.9.2244');
+define('ROSTER_VERSION','2.0.9.2249');
 
 /**
  * Roster Conf File
