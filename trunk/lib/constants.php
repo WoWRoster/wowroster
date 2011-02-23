5252da5eb29ed5320933ec88c41e082bc4a95a7f
c.treyce@gmail.com <c.treyce@gmail.com@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2011-02-23 01:52:37 +0000

    Roster Trunk: - Bump the ver cuz I gotta!

--- trunk/lib/constants.php
+++ trunk/lib/constants.php
@@ -18,7 +18,7 @@ if( !defined('IN_ROSTER') )
 	exit('Detected invalid access to this file!');
 }
 
-define('ROSTER_VERSION', '2.0.9.2276');
+define('ROSTER_VERSION', '2.0.9.2279');
 
 /**
  * Roster Conf File
