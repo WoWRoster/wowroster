a97956acdeebf1af4a2147550a391070ba218561
ulminia@gmail.com <ulminia@gmail.com@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2012-02-15 18:21:39 +0000

    Roster Trunk - NEW core addons added -- CMS News - cms like style news page with banners -- Forums - a simple forum for wowroster

---
+++ branches/newaccess/addons/main/locale/esES.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * WoWRoster.net WoWRoster
+ *
+ *
+ * @copyright  2002-2011 WoWRoster.net
+ * @license    http://www.gnu.org/licenses/gpl.html   Licensed under the GNU General Public License v3.
+ * @version    SVN: $Id$
+ * @link       http://www.wowroster.net
+ * @package    News
+ * @subpackage Locale
+*/
+
+// -[ esES Localization ]-
+
+// Installer names
+$lang['news']			        = 'Noticias';
+$lang['news_desc']			    = 'Sistema de noticias, con comentarios';
+
+// Button names
+$lang['news_button']			= 'Noticias|Publica artículos y temas de interés';
+
+$lang['no_news']				= 'No hay noticias disponibles';
+$lang['bad_news_id']			= 'Invalido ID de noticias';
+
+$lang['add_news']               = 'Añadir noticias';
+$lang['enable_html']            = 'Activar HTML';
+$lang['disable_html']           = 'Desactivar HTML';
+$lang['posted_by']              = 'Publicado por';
+$lang['edit']                   = 'Editar';
+$lang['edit_news']              = 'Editar noticias';
+$lang['comments']               = 'Comentarios';
+$lang['add_comment']            = 'Añadir comentario';
+$lang['edit_comment']           = 'Editar comentario';
+$lang['n_comment']              = '%s comentario';
+$lang['n_comments']             = '%s comentarios';
+
+$lang['b_title'] = 'Title';
+$lang['b_desc'] = 'Desc';
+$lang['b_url'] = 'Link';
+$lang['b_image'] = 'Image File';
+$lang['b_add'] = 'Add Banner';
+
+$lang['news_edit_success']      = 'La noticia se ha editado satisfactoriamente';
+$lang['news_add_success']       = 'La noticia se ha añadido satisfactoriamente';
+$lang['banner_add_success']       = 'Banner added successfully';
+$lang['news_error_process']     = 'Ha surgido un problema procesando el artículo';
+
+$lang['comment_edit_success']   = 'El comentario se ha editado satisfactoriamente';
+$lang['comment_add_success']    = 'El comentario se ha añadido satisfactoriamente';
+$lang['comment_error_process']  = 'Ha surgido un problema procesando el comentario';
+
+// Config strings
+$lang['admin']['cmsmain_conf']		= 'Configurar noticias|Configuración de las noticias';
+$lang['admin']['cmsmain_banner'] = 'Banners';
+$lang['admin']['cmsmain_banneradd'] = 'Add Banners';
+$lang['admin']['news_add']		= 'Añadir noticias|Elige el nivel mínimo necesario para añadir una noticia.';
+$lang['admin']['news_edit']		= 'Editar noticias|Elige el nivel mínimo necesario para editar una noticia.';
+$lang['admin']['comm_add']		= 'Añadir comentarios|Elige el nivel mínimo necesario para añadir un comentario.';
+$lang['admin']['comm_edit']		= 'Editar comentarios|Elige el nivel mínimo necesario para editar un comentario.';
+$lang['admin']['news_html']		= 'Noticias HTML|Activa o desactiva HTML en las noticias predeterminadamente, o desactiva HTML definitivamente.<br />Esto no afecta a las noticias existentes';
+$lang['admin']['comm_html']		= 'Comentario HTML|Activa o desactiva HTML en los comentarios predeterminadamente, o desactiva HTML definitivamente.<br />Esto no afecta a los comentarios existentes';
+$lang['admin']['news_nicedit']  = 'Nicedit caja de texto|Permite activar o desactivar la caja de texto Nicedit.';
