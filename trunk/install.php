66c5ef37a41814cf6a5f4638be56c0990130bf48
ulminia@gmail.com <ulminia@gmail.com@1a6307ba-3d0c-11df-bab5-273325bd80cc>
2012-02-16 18:21:06 +0000

    Roster Trunk - Installer changes for new user system

--- trunk/install.php
+++ trunk/install.php
@@ -857,7 +857,7 @@ function process_step4( )
 	('Guild'),
 	('Officer'),
 	('Admin');");
-	$db->query("UPDATE `" . $db->table('user_members') . "` SET `pass` = '" . $pass_word . "' AND `access` = '11:0' WHERE `usr` = 'Admin';");  
+	$db->query("UPDATE `" . $db->table('user_members') . "` SET `pass` = '" . $pass_word . "',`access` = '11:0' WHERE `usr` = 'Admin';");  
 
 	$tpl->message_append('The WoWRoster Officer and Guild accounts have been set to the same password and access as the admin account<br />Please change these passwords via RosterCP-&gt;Change Password');    
 
